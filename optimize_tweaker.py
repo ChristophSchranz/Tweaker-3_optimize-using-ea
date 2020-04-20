2127cdee656df5f32456458e304574003c1f9863
christoph.schranz@salzburgresearch.at <christoph.schranz@salzburgresearch.at>
2020-04-20 12:16:55 +0200

    New branch with updated functions

--- optimize_tweaker.py
+++ optimize_tweaker.py
@@ -16,19 +16,25 @@ from tweaker_phenotype import evaluate_tweaker
 # 2020-04-11 = [0.00132, 0.1035, 0.1055, 2.44, 340.0, 0.1109, 0.52] (2.1221719828604946, 1.75)
 # 2020-04-13 [0.001451, 0.291153, 0.028855, 1.400084, 669.452018, 1.566669, 0.088707] (1.7734233947572995, 1.5)
 # 2020-04-14: [0.001241, 0.220733, 0.027727, 1.396286, 222.797949, 0.368569, 0.145722] (1.8986392001671926, 1.5)
+# From here on, it is a 0.25 harder.
+# 2020-04-16: [1.0949166432153108, 1.3801130552407321, 4.212270839789636, 2.9124195997415634, -0.12123768217392668, 1.9189241901924767, 0.851190555042431, 1.0501212774303932, 0.0619619118477204, 0.3218715070457661, 2.2793963527249073, -0.0006922836147128827, 0.4803757083992928, 0.02205278899619667, 0.00031301424728405193,
+# 0.30571098484924314, 0.09500435168604665, 0.42768694157524495, 0.010720139799460952] (2,28859922694892, 2.0)
+# 2020-04-18: ... (7, 6.25)
+# 2020-04-19: ... (9, 8)
 
-chromosome_mapping = [("ABSOLUTE_F", 100.0), ("RELATIVE_F", 1.0), ("CONTOUR_F", 1.0), ("FIRST_LAY_H", 0.1),
-                    ("TAR_A", 1.0), ("TAR_B", 0.01), ("TAR_C", 1.0), ("TAR_D", 1.0), ("BOTTOM_F", 1),
-                    ("PLAFOND_ADV_A", 0.01), ("PLAFOND_ADV_B", 0.2), ("PLAFOND_ADV_C", 0.01),
-                    ("ANGLE_SCALE", 0.1), ("ASCENT", 0.1), ("NEGL_FACE_SIZE", 1.0), ("CONTOUR_AMOUNT", 0.01)]
-chromosome_dict = dict(chromosome_mapping)
+chrome_map = [("ABS_F", lambda x: 0.01+0.001*x), ("REL_F", lambda x: 1.2+0.1*x), ("CONTOUR_F", lambda x: 1.1+0.1*x),
+              ("FIRST_LAY_H", lambda x: 0.1+0.01*x), ("TAR_B", lambda x: 0.2+0.01*x), ("TAR_C", lambda x: 1+0.1*x),
+              ("TAR_D", lambda x: 1+0.1*x), ("BOTTOM_F", lambda x: 1.1+0.1*x),
+              ("PLAF_A", lambda x: 0.01+0.001*x), ("PLAF_B", lambda x: 0.22+0.02*x), ("PLAF_C", lambda x: 1.0+0.001*x),
+              ("ANGLE_SCALE", lambda x: 0.7336+0.005*x), ("ASCENT", lambda x: 116+1.2*x),
+              ("NEGL_FACE_SIZE", lambda x: 0.41+0.01*x), ("CONTOUR_AMOUNT", lambda x: 0.01+0.001*x),
+              ("OV_B", lambda x: 53+8*x)]
+chrome_dict = dict(chrome_map)
+CHROMOSOMES = [chrome[0] for chrome in chrome_map]
 
-# CHROMOSOMES = ["VECTOR_TOL", "PLAFOND_ADV", "FIRST_LAY_H", "NEGL_FACE_SIZE", "ABSOLUTE_F", "RELATIVE_F", "CONTOUR_F"]
-CHROMOSOMES = [chrome[0] for chrome in chromosome_mapping]
-
-n_individuals = 10  # 25 was good
-n_generations = 5
-n_objects = 5
+n_individuals = 5  # 25 was good
+n_generations = 20
+n_objects = 100
 
 
 # Create class to store  model-wise statistics about errors and miss-classifications
@@ -47,9 +53,9 @@ class StatPos:
 
 
 stat_pos = StatPos()
-statistics = pd.DataFrame(0.0, index=range(n_individuals * n_generations),
-                          columns=sorted([f"Model{i}.stl_error" for i in range(1, n_objects+1)] +
-                                         [f"Model{i}.stl_miss" for i in range(1, n_objects+1)]))
+stats = pd.DataFrame(0.0, index=range(n_individuals * n_generations),
+                     columns=sorted([f"Model{i}.stl_error" for i in range(1, n_objects + 1)] +
+                                    [f"Model{i}.stl_miss" for i in range(1, n_objects + 1)]))
 
 # Read reference file that holds all grades for the models
 ref_file = os.path.join("data", "ref_fitness.json")
@@ -88,7 +94,7 @@ def evaluate(individual, verbose=False, is_phenotype=False):
     error = 0
     miss = 0
     # iterate through multiple objects and compare to real values
-    for model_number, model in enumerate(ref["models"][:n_objects+1]):
+    for model_number, model in enumerate(ref["models"][:n_objects + 1]):
         error_per_model = 0
         miss_per_model = 0
         # extract the filename and run the tweaker
@@ -110,15 +116,15 @@ def evaluate(individual, verbose=False, is_phenotype=False):
                     miss_per_model += 0
                 elif ref_a['grade'] == "B":
                     miss_per_model += 0.25  # Here the error can be lower, as B ist still a good value
-                    statistics.loc[stat_pos.get()][f"{model['name']}_miss"] = 0.25
+                    stats.loc[stat_pos.get()][f"{model['name']}_miss"] = 0.25
                 elif ref_a['grade'] == "C":
                     miss_per_model += 1
-                    statistics.loc[stat_pos.get()][f"{model['name']}_miss"] = 1
+                    stats.loc[stat_pos.get()][f"{model['name']}_miss"] = 1
                 break
         # Add error if alignment is not found
         if not referred_flag:
             miss_per_model += 1
-            statistics.loc[stat_pos.get()][f"{model['name']}_miss"] = 1
+            stats.loc[stat_pos.get()][f"{model['name']}_miss"] = 1
 
         # Weight the values of the unprintablities
         for alignment in result.best_5:
@@ -135,12 +141,13 @@ def evaluate(individual, verbose=False, is_phenotype=False):
                         if ref_a['grade'] == "A":
                             referred_value = 0
                         elif ref_a['grade'] == "B":
-                            referred_value = 1/2
+                            referred_value = 1 / 2
                         elif ref_a['grade'] == "C":
                             referred_value = 1
                         break
                 # Increase the error, compute the squared residual and normalize with 1/(|results|*|n_objects|
-                inc = 2/(len(result.best_5)*n_objects) * (referred_value - 1/(1 + np.exp(0.5*(10-alignment[4]))))**2
+                inc = 2 / (len(result.best_5) * n_objects) * (
+                            referred_value - 1 / (1 + np.exp(0.5 * (10 - alignment[4])))) ** 2
                 error_per_model += inc
                 # Adding high error on negative unprintability
                 if alignment[4] < 0.0:
@@ -150,7 +157,7 @@ def evaluate(individual, verbose=False, is_phenotype=False):
         # normalized with 0.5/|n_objects|
         error_per_model += 1/n_objects * 1/(1 + np.exp(0.5*(10-result.unprintability)))
 
-        statistics.loc[stat_pos.get()][f"{model['name']}_error"] = error_per_model
+        stats.loc[stat_pos.get()][f"{model['name']}_error"] = error_per_model
         error += error_per_model
         miss += miss_per_model
 
@@ -168,22 +175,21 @@ def map_parameters(name, allele):
     :param allele: value for the specified gene
     :return: value that can be used by the algorithm
     """
-    return chromosome_dict[name] * allele
+    return chrome_dict[name](allele)
 
 
 def map_all_parameters(chromosome, exact=False):
     """
-    This functions maps each allel of the chromosome that is around 1 into the appropriate scales.
+    This functions maps each allele of the chromosome that is around 1 into the appropriate scales.
     Therefore, it maps the genotype to the phenotype.
     :param exact: rounds the chromosomes to 6 digits if true
     :param chromosome: chromosome that contains all genes
     :return: list of the real values
     """
     if exact:
-        return [chromosome[i] * allele[1] for i, allele in enumerate(chromosome_mapping)]
+        return [allele[1](chromosome[i]) for i, allele in enumerate(chrome_map)]
     else:
-        return [round(chromosome[i] * allele[1], 6) for i, allele in enumerate(chromosome_mapping)]
-
+        return [float("%.4g" % allele[1](chromosome[i])) for i, allele in enumerate(chrome_map)]
 
 
 if __name__ == "__main__":
@@ -201,11 +207,13 @@ if __name__ == "__main__":
     # allow parallel computing.
     # These lines must be within the __main__ and the program must be started with a "-m scoop" parameter.
     from scoop import futures
+
     toolbox.register("map", map)
 
 
     def about_one():
-        return np.random.normal(1, 0.2)
+        return np.random.normal(0, 3)
+
 
     # Draw random variables for the initial population
     toolbox.register("attr_float", random.random)
@@ -218,24 +226,25 @@ if __name__ == "__main__":
     # Define the genetic operations
     toolbox.register("evaluate", evaluate)
     toolbox.register("mate", tools.cxTwoPoint)
-    toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=0.25, indpb=0.75)  # sigma of 0.25 is the best
+    toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=1, indpb=0.75)  # sigma of 0.25 is the best
     toolbox.register("select", tools.selTournament, tournsize=3)
 
-    # Create hall of fame of size ceiling(2.5%)
-    hall_of_fame = tools.HallOfFame(maxsize=int(n_individuals * 0.025) + 1)
+    # Create hall of fame of size 1
+    hall_of_fame = tools.HallOfFame(maxsize=1)
 
     # Create a population and update the history
     population = toolbox.population(n=n_individuals)
 
-    df = pd.DataFrame(index=range(1, n_generations+1), columns=["top", "median", "best"])
+    today = datetime.now().date().isoformat()
+    df = pd.DataFrame(index=range(1, n_generations + 1), columns=["top", "median", "best"])
     df.index.name = "gen"
     fittest_ever = None
-    for gen in range(1, n_generations+1):
+    for gen in range(1, n_generations + 1):
         print(f"Generation {gen} of {n_generations}:")
         offspring = algorithms.varAnd(population, toolbox, cxpb=0.5, mutpb=0.4)
         fits = toolbox.map(toolbox.evaluate, offspring)
         for fit, ind in zip(fits, offspring):
-            print(f"The phenotype {map_all_parameters(ind)} \t has a fitness of: {round(fit[0], 6)}")
+            print(f"The phenotype {map_all_parameters(ind)} \t has a fitness of: ({float('%.6g' % fit[0])}, {fit[1]})")
             ind.fitness.values = fit
 
         # Clone the best individuals from hall_of_fame into the offspring for the selection
@@ -252,16 +261,17 @@ if __name__ == "__main__":
         df.loc[gen]["median"] = np.median([ind.fitness.values[0] for ind in population])
         df.loc[gen]["best"] = map_all_parameters(hall_of_fame[0], exact=True)
 
+        if gen % 10 == 0:
+            print(os.path.join("data", f"{today}_{n_generations}gen_{n_individuals}ind_{n_objects}obj-df.csv"))
+            df.to_csv(os.path.join("data", f"{today}_{n_generations}gen_{n_individuals}ind_{n_objects}obj-df.csv"))
+            stats.to_csv(os.path.join("data", f"{today}_{n_generations}gen_{n_individuals}ind_{n_objects}"
+                                              f"obj-stats.csv"))
         print(f"Best phenotype so far is: {map_all_parameters(hall_of_fame[0])} with a fitness of "
-              f"{round(hall_of_fame[0].fitness.values[0], 6)}.\n")
+              f"{hall_of_fame[0].fitness.values}.\n")
 
-    df.to_csv(os.path.join("data", f"{datetime.now().date().isoformat()}_"
-                                   f"{n_generations}gen_{n_individuals}ind_{n_objects}obj-df.csv"))
     print(df)
+    print(stats.head())
 
-    statistics.to_csv(os.path.join("data", f"{datetime.now().date().isoformat()}_"
-                                           f"{n_generations}gen_{n_individuals}ind_{n_objects}obj-stats.csv"))
-    print(statistics.head())
-
-    results = evaluate(hall_of_fame[0], verbose=True)
+    results = evaluate(hall_of_fame[0], verbose=False)
+    print(f"Best phenotype is {map_all_parameters(hall_of_fame[0])}")
     print(results)
